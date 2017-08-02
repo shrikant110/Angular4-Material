500532c85841be21aadef1ff830489da39eb42b9
shrikant110 <shrikant110@gmail.com>
2017-08-02 23:32:06 +0530

    Source Code
    Add Code

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  myData: Array<any>;
+
+  constructor(private http:Http) {
+    this.myData=[
+  {
+    "albumId": 1,
+    "id": 1,
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+    "url": "http://placehold.it/600/92c952",
+    "thumbnailUrl": "http://placehold.it/150/92c952"
+  },
+  {
+    "albumId": 1,
+    "id": 2,
+    "title": "reprehenderit est deserunt velit ipsam",
+    "url": "http://placehold.it/600/771796",
+    "thumbnailUrl": "http://placehold.it/150/771796"
+  }];
+    //this.http.get('https://jsonplaceholder.typicode.com/photos')
+    //  .map(response => response.json())
+    //  .subscribe(res => this.myData = res);
+
+  }
+}
